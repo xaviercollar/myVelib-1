4ca0ae7c5b2635c402c261770f308cf2416d4559
xaviercollar <36864449+xaviercollar@users.noreply.github.com>
2018-03-05 15:19:29 +0100

    Update RidePolicy.java

--- Velib2/src/ridePolicies/RidePolicy.java
+++ Velib2/src/ridePolicies/RidePolicy.java
@@ -1,5 +1,7 @@
 package ridePolicies;
 
 public interface RidePolicy {
-
+	public Station computeStart(Reseau reseau,String typevelo,GPScoord start, GPScoord end);
+	public Station computeEnd(Reseau reseau,String typevelo,GPScoord start, GPScoord end);
 }
+
